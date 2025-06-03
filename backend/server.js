b8c2b5851616cfb8e8adf858ffab6b9d7b14fe0e
SergeyKrimon3 <krimonsergey@gmail.com>
2025-06-02 22:11:55 -0300

    Att Backend Files

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,15 @@
-//JSON Server Module
+// JSON Server Module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("database.json");
-
-//Default
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
 server.use(
     jsonServer.rewriter({
-        "/*" : "/$1",
+        "/*": "/$1",
     })
 );
 server.use(router);
 
-server.listen(3001, () => {
-    console.log("JSON Server is running!");
-});
-
-//Server API
 module.exports = server;
