a2e574e45160ef5ce897c6c89c5f639ad82a68ee
SergeyKrimon3 <krimonsergey@gmail.com>
2025-06-04 17:53:30 -0300

    Changed Url RestEndpoint

--- Frontend/src/constants/rest-endpoint.cosntants.ts
+++ Frontend/src/constants/rest-endpoint.cosntants.ts
@@ -1,10 +1,12 @@
+export const url = "https://3597-2804-1b3-a9c0-2e86-b365-3cf3-ffb4-963b.ngrok-free.app" 
+
 export const RestEndpoint = {
     tasks: {
-        getAllTasks: "/api/tasks",
-        createTasks: "/api/tasks",
-        getTaskById: (taskId: string) => `/api/tasks/${taskId}`,
-        updateTaskById: (taskId: string) => `/api/tasks/${taskId}`,
-        deleteTask: (taskId: string) => `/api/tasks/${taskId}`,
-        updateStatusTask: (taskId: string) => `/api/tasks/${taskId}`
+        getAllTasks: url + "/api/tasks",
+        createTasks: url + "/api/tasks",
+        getTaskById: (taskId: string) => url + `/api/tasks/${taskId}`,
+        updateTaskById: (taskId: string) => url + `/api/tasks/${taskId}`,
+        deleteTask: (taskId: string) => url + `/api/tasks/${taskId}`,
+        updateStatusTask: (taskId: string) => url + `/api/tasks/${taskId}`
     }
 }
