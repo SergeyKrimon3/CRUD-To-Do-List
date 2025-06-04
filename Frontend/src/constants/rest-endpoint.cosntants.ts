ebe722716fb6e943f770197addefdabdb308a9d6
SergeyKrimon3 <krimonsergey@gmail.com>
2025-06-04 17:57:28 -0300

    Remove /api

--- Frontend/src/constants/rest-endpoint.cosntants.ts
+++ Frontend/src/constants/rest-endpoint.cosntants.ts
@@ -2,11 +2,11 @@ export const url = "https://3597-2804-1b3-a9c0-2e86-b365-3cf3-ffb4-963b.ngrok-fr
 
 export const RestEndpoint = {
     tasks: {
-        getAllTasks: url + "/api/tasks",
-        createTasks: url + "/api/tasks",
-        getTaskById: (taskId: string) => url + `/api/tasks/${taskId}`,
-        updateTaskById: (taskId: string) => url + `/api/tasks/${taskId}`,
-        deleteTask: (taskId: string) => url + `/api/tasks/${taskId}`,
-        updateStatusTask: (taskId: string) => url + `/api/tasks/${taskId}`
+        getAllTasks: url + "/tasks",
+        createTasks: url + "/tasks",
+        getTaskById: (taskId: string) => url + `/tasks/${taskId}`,
+        updateTaskById: (taskId: string) => url + `/tasks/${taskId}`,
+        deleteTask: (taskId: string) => url + `/tasks/${taskId}`,
+        updateStatusTask: (taskId: string) => url + `/tasks/${taskId}`
     }
 }
