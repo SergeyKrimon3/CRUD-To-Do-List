a5390528dc2dbd210a7fdb6ccc8a066511698271
SergeyKrimon3 <krimonsergey@gmail.com>
2025-06-12 21:47:55 -0300

    Changed new ngrok target

--- Frontend/src/constants/rest-endpoint.cosntants.ts
+++ Frontend/src/constants/rest-endpoint.cosntants.ts
@@ -1,4 +1,4 @@
-export const url = "https://416b-2804-1b3-a9c0-2e86-d480-128b-6fd2-53.ngrok-free.app" 
+export const url = "https://09d7-2804-1b3-a9c0-2e86-d480-128b-6fd2-53.ngrok-free.app" 
 
 export const RestEndpoint = {
     tasks: {
